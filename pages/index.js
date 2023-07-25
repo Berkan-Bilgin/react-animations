7331ba82618ff6ee471fa71a70f5dcb37b762b3c
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-07-25 14:51:20 +0300

    added services and contact component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Main from "@/components/Main";
+import Services from "@/components/Services";
 import React from "react";
+
+
+const images = [
+  "/images/left monkey.png",
+  "/images/right monkey.png",
+ 
+];
 
 export default function index() {
   return (
     <div>
-      <Main id="anasayfa" />
-      <About id="hakkinda" />
+      <Main  />
+      <About  />     
+      <Services />
+      <Contact images={images} starCropWidth='50%' />
+     
     </div>
   );
 }
