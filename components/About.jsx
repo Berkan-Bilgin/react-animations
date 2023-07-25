d94ca191afe6d80fa554557a2ba00b4ffbb446e5
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-07-25 14:48:47 +0300

    added responsive classes

--- components/About.jsx
+++ components/About.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="h-[800px] flex justify-center overflow-x-hidden">
+    <div id='about' className="h-[800px] flex justify-center overflow-x-hidden">
       <div data-aos="slide-right" data-aos-duration="2000">
         <Image
           src="/images/left monkey.png"
@@ -13,16 +13,16 @@ export default function About() {
         />
       </div>
 
-      <div className="text-red-700 text-center mt-20">
+      <div className="text-red-700 text-center mt-4 md:mt-10 sm:mt-2">
         <h1
-          className="font-serif text-9xl"
+          className="font-serif text-5xl lg:text-9xl md:text-7xl"
           data-aos="fade-up"
           data-aos-duration="2000"
         >
           MINT
         </h1>
         <h2
-          className="font-serif text-9xl"
+          className="font-serif text-5xl lg:text-9xl md:text-7xl"
           data-aos="fade-up"
           data-aos-duration="2000"
           data-aos-delay="500"
