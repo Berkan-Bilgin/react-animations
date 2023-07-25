2d022215c6811f1484592876c36531e1a527f89c
Berkan Bilgin <berkan.bilgin.dev@gmail.com>
2023-07-25 14:49:20 +0300

    created contact page

---
+++ components/Contact.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+const Contact = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  const currentImage = images[currentImageIndex];
+
+  return (
+    <div className="w-full h-screen bg-center bg-no-repeat bg-contain flex items-start justify-center" 
+    style={{ backgroundImage: `url('https://chainzoku.io/_nuxt/image/fa5785.webp')` }}
+    >
+   <div className="mt-20">
+  <Image 
+    src={currentImage} 
+    alt={`Image ${currentImageIndex + 1}`} 
+    width={500} 
+    height={300} 
+  />
+</div>
+    </div>
+  );
+};
+
+export default Contact;
